35b0d92b9ef7fc2fe596669ed17d4b2230eb0b2f
Dan <peritract@hotmail.co.uk>
2024-08-15 10:09:20 +0100

    Simple function

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 """Something descriptive about the project."""
 
+def despair(name: str) -> str:
+    """Returns an accurate assessment."""
+    return f"I'm very disappointed in you, {name}."
+
 if __name__ == "__main__":
 
-    pass
+    print(despair("Megan"))
