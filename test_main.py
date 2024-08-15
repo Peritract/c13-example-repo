5a53509ab5cdb469e889cfda8bf1c0b2c9dd606e
Dan <peritract@hotmail.co.uk>
2024-08-15 10:58:33 +0100

    De-cruelify

--- test_main.py
+++ test_main.py
@@ -1,8 +1,8 @@
 """Tests for the main file."""
 
-from main import despair
+from main import assess
 
 def test_we_are_correctly_viewing_caleb() -> None:
     """Checks that the function handles normal input as expected."""
 
-    assert despair("Caleb") == "I'm very disappointed in you, Caleb."
+    assert assess("Caleb") == "I'm very disappointed in you, Caleb."
