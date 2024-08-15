e4114bb2e18ee8930d28075c3cb9e605ef2dfa8c
Dan <peritract@hotmail.co.uk>
2024-08-15 10:49:48 +0100

    Add docstring

--- test_main.py
+++ test_main.py
@@ -2,6 +2,7 @@
 
 from main import despair
 
-def test_we_are_correctly_viewing_caleb():
+def test_we_are_correctly_viewing_caleb() -> None:
+    """Checks that the function handles normal input as expected."""
 
     assert despair("Caleb") == "I'm very disappointed in you, Caleb."
